57484340edcd7f4a6d8abd8ac2be98216ecc9be3
PAPPURAJ <pappuraj.duet@gmail.com>
2021-05-06 13:16:04 +0600

    First presentation

---
+++ includes/sidebar.php
@@ -0,0 +1,37 @@
+<div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
+    <div class="panel-heading"><!-- panel-heading Begin -->
+        <h3 class="panel-title">Products Categories</h3>
+    </div><!-- panel-heading Finish -->
+    
+    <div class="panel-body"><!-- panel-body Begin -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Begin -->
+            
+            <li><a href="#">SSD</a></li>
+            <li><a href="#">Laptop</a></li>
+            <li><a href="#">Mobile Phone</a></li>
+            <li><a href="#">Bag</a></li>
+            <li><a href="#">Arduino</a></li>
+            
+        </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
+    </div><!-- panel-body Finish -->
+    
+</div><!-- panel panel-default sidebar-menu Finish -->
+
+
+<div class="panel panel-default sidebar-menu"><!-- panel panel-default sidebar-menu Begin -->
+    <div class="panel-heading"><!-- panel-heading Begin -->
+        <h3 class="panel-title">Categories</h3>
+    </div><!-- panel-heading Finish -->
+    
+    <div class="panel-body"><!-- panel-body Begin -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Begin -->
+            
+            <li><a href="#">Electronics</a></li>
+            <li><a href="#">Growsary</a></li>
+            <li><a href="#">Bags</a></li>
+            <li><a href="#">Others</a></li>
+            
+        </ul><!-- nav nav-pills nav-stacked category-menu Finish -->
+    </div><!-- panel-body Finish -->
+    
+</div><!-- panel panel-default sidebar-menu Finish -->
